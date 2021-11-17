56e248411d38f81cfa494407d92c2b23ac84a191
Evgeny Rinkevich <rinkevich2000@gmail.com>
2021-11-17 10:56:31 +0300

    Homework Lesson_1

---
+++ mainapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'mainapp/index.html')
+
+
+def about(request):
+    return render(request, 'mainapp/about.html')
+
+
+def products(request):
+    return render(request, 'mainapp/products.html')
+
+
+def contact(request):
+    return render(request, 'mainapp/contact.html')
