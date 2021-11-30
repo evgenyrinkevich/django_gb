fdd831900454a462f771b98debdabc70a5ad3ac0
evgenyrinkevich <rinkevich2000@gmail.com>
2021-11-30 12:29:30 +0300

    Lesson 4
    -authapp
    -fill db command

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+JSON_PATH = 'mainapp/json'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
---
+++ mainapp/management/commands/fill_db.py
@@ -0,0 +1,32 @@
+import os
+import json
+
+from django.core.management import BaseCommand
+from mainapp.models import Product, ProductCategory
+from django.conf import settings
+
+
+def load_from_json(file_name):
+    with open(os.path.join(settings.JSON_PATH, f'{file_name}.json'), 'r', encoding='utf-8') as infile:
+        return json.load(infile)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        categories = load_from_json('product_categories')
+
+        ProductCategory.objects.all().delete()
+        [ProductCategory.objects.create(**category) for category in categories]
+
+        products = load_from_json('products')
+        Product.objects.all().delete()
+        for product in products:
+            category_name = product['category']
+            _category = ProductCategory.objects.filter(name=category_name).first()
+            product['category'] = _category
+            new_product = Product(**product)
+            new_product.save()
+
+        # super_user = User.objects.create_superuser('django', 'django@gb.ru', 'geekbrains')
+
