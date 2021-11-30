f4804bac2ecb7f5d50001cc60de44d7fe878fbc3
evgenyrinkevich <rinkevich2000@gmail.com>
2021-11-30 17:54:25 +0300

    Lesson 4
    -authapp
    -fill db command
    - ShopUser model

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -132,6 +132,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 JSON_PATH = 'mainapp/json'
 
+AUTH_USER_MODEL = 'authapp.ShopUser'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
