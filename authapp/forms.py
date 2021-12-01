a6eea95a4cb307fe1b9c5e79034c375b520f805d
evgenyrinkevich <rinkevich2000@gmail.com>
2021-12-01 09:32:16 +0300

    Lesson 4
    -authapp
    -fill db command (updated)
    - ShopUser model
    - login / logout
    - user registration

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from authapp.models import ShopUser
 
@@ -12,3 +12,17 @@ class ShopUserLoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = f'form-control {field_name}'
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    class Meta:
+        model = ShopUser
+        fields = (
+            'username', 'password1', 'password1', 'email', 'first_name', 'last_name', 'age', 'avatar'
+        )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = f'form-control {field_name}'
+            field.help_text = ''
--- authapp/views.py
+++ authapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from authapp.forms import ShopUserLoginForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm
 
 
 def login(request):
@@ -29,3 +29,18 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('main:index'))
+
+
+def register(request):
+    if request.method == 'POST':
+        form = ShopUserRegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        form = ShopUserRegisterForm()
+    context = {
+        'title': 'registration',
+        'form': form
+    }
+    return render(request, 'authapp/register.html', context)
