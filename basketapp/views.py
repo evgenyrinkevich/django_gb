9c7a91faf4ce8e60760921d9ce57b5364028f6ea
evgenyrinkevich <rinkevich2000@gmail.com>
2021-12-07 10:13:53 +0300

    Lesson 6
    - login_required

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
@@ -5,6 +6,7 @@ from basketapp.models import Basket
 from mainapp.models import Product
 
 
+@login_required
 def add(request, pk):
     product = get_object_or_404(Product, pk=pk)
 
