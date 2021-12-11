1e4f3b608208948ecae45cca8a5cecbf9f184be0
evgenyrinkevich <rinkevich2000@gmail.com>
2021-12-11 21:26:02 +0300

    Lesson_7 - adminapp   - index page   - link to admin added   - users CRUD

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -6,4 +6,7 @@ app_name = 'adminapp'
 
 urlpatterns = [
     path('', adminapp.index, name='index'),
+    path('user/create/', adminapp.user_create, name='user_create'),
+    path('user/update/<int:pk>/', adminapp.user_update, name='user_update'),
+    path('user/delete/<int:pk>/', adminapp.user_delete, name='user_delete'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
+from adminapp.forms import AdminShopUserCreateForm, AdminShopUserUpdateForm
 from authapp.models import ShopUser
 
 
@@ -13,3 +16,55 @@ def index(request):
     }
 
     return render(request, 'adminapp/index.html', context)
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def user_create(request):
+    if request.method == 'POST':
+        user_form = AdminShopUserCreateForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('my_admin:index'))
+    else:
+        user_form = AdminShopUserCreateForm()
+
+    context = {
+        'title': 'create user',
+        'form': user_form}
+
+    return render(request, 'adminapp/user_update.html', context)
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def user_update(request, pk):
+    user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        user_form = AdminShopUserUpdateForm(request.POST, request.FILES, instance=user)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('my_admin:index'))
+    else:
+        user_form = AdminShopUserUpdateForm(instance=user)
+
+    context = {
+        'title': 'update user',
+        'form': user_form}
+
+    return render(request, 'adminapp/user_update.html', context)
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def user_delete(request, pk):
+    user = get_object_or_404(ShopUser, pk=pk)
+
+    if request.method == 'POST':
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse('my_admin:index'))
+
+    context = {
+        'title': 'delete user',
+        'user_to_delete': user
+    }
+
+    return render(request, 'adminapp/user_delete.html', context)
